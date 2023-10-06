7bfc624707f3f825e9bbf34a837447cafda95bb3
Zanshin07 <23cm0107@jec.ac.jp>
2023-10-06 10:12:59 +0900

    Initial Commit

---
+++ Festival07/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Festival07
+//
+//  Created by cmStudent on 2023/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Festival07/Festival07App.swift
@@ -0,0 +1,17 @@
+//
+//  Festival07App.swift
+//  Festival07
+//
+//  Created by cmStudent on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct Festival07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
